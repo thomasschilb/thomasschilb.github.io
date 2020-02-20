4bd8dc45f91f5b69d0eefb45639ce8252e33a90b
Thomas Schilb <33239073+thomasschilb@users.noreply.github.com>
2020-02-20 15:19:33 +0100

    TS | Projects

---
+++ Web/2020/~thomasschilb/thomasschilb.2020.v7.1rev68/logout.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+    session_start();
+    // Destroy session
+    if(session_destroy()) {
+        // Redirecting To Home Page
+        header("Location: index.php");
+    }
+*/
+
+?><meta http-equiv="refresh" content="0; URL=index.php"><?
+
+?>
---
+++ Web/2020/~thomasschilb/thomasschilb.2020.v7.1rev68/lost+found/links-xdcc.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta content="de" http-equiv="Content-Language">
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<title>Thomas Schilb | TS</title>
+<style>
+
+/* BODY */
+
+body {
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+	}
+
+/* HYPERLINKS */
+
+a {
+	color: #808080;
+	text-decoration: none;
+}
+a:visited {
+	color: #808080;
+}
+a:active {
+	color: #808080;
+}
+a:hover {
+	color: #31C8F9;
+}
+
+/* TABLE */
+
+#tstable {
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+}
+
+#tstable td, #tstable th {
+  border: 0px solid #333333;
+  padding: 8px;
+}
+
+#tstable tr:nth-child(even){background-color: #202020;}
+
+#tstable tr:hover {background-color: #444444;}
+
+#tstable th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #333333;
+  color: white;
+}
+
+/* FONT, MISC */
+
+.font-normal {
+	font-weight: normal;
+}
+
+.text-align-right {
+	text-align: right;
+}
+
+
+.table-footer-center {
+	text-align: center;
+	color: #FFFFFF;
+}
+
+
+.text-underline {
+	text-decoration: underline;
+}
+
+
+.table-footer {
+	color: #FFFFFF;
+	text-align: left;
+}
+
+
+.auto-style1 {
+	text-align: center;
+}
+
+
+</style>
+</head>
+
+<body style="margin: 0; color: #808080; background-color: #000000">
+<p class="table-footer-center">L I N K S : X D C C</p>
+<p><font color="#31C8F9">Site [5]</font></p>
+<table cellspacing="0" id="tstable" width="100%">
+	<tr>
+		<th><span class="font-normal">Name</span></th>
+		<th><span class="font-normal"></span></th>
+	</tr>
+	<tr>
+		<td>ixirc.com</td>
+		<td class="text-align-right"><a href="http://ixirc.com" target="_blank"><img height="16" src="img/www.png" width="16"></a></td>
+	</tr>
+	<tr>
+		<td>sunxdcc.com</td>
+		<td class="text-align-right">
+		<a href="http://sunxdcc.com" target="_blank"><img height="16" src="img/www.png" width="16"></a></td>
+	</tr>
+	<tr>
+		<td>xdcc.it</td>
+		<td class="text-align-right"><a href="http://xdcc.it" target="_blank"><img height="16" src="img/www.png" width="16"></a></td>
+	</tr>
+	<tr>
+		<td>xdcc.eu</td>
+		<td class="text-align-right"><a href="http://xdcc.eu" target="_blank"><img height="16" src="img/www.png" width="16"></a></td>
+	</tr>
+	<tr>
+		<td>xdcc.me</td>
+		<td class="text-align-right"><a href="http://xdcc.me"><img height="16" src="img/www.png" width="16"></a></td>
+	</tr>
+	</table>
+<p class="auto-style1"><em><font size="2">Comment: "XDCC - IRC-Packets Search 
+Engine."</font></em></p>
+<p>&nbsp;</p>
+
+</body>
+
+</html>
